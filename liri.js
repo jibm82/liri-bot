409b55e2990e0b68e53bbcae032d9a314f48c80e
Jorge Baez <jibm_82@yahoo.com.mx>
2018-10-24 15:16:49 -0500

    Integrated Spotify API

--- liri.js
+++ liri.js
@@ -5,17 +5,44 @@ const keys = require("./keys");
 
 let command = process.argv[2];
 let params = process.argv.slice(3).join(" ");
-let bandsInTownRequestUrl = `https://rest.bandsintown.com/artists/${params}/events?app_id=${keys.bandsInTown.appId}`;
-console.log(params);
 
-request(bandsInTownRequestUrl, (error, response, body) => {
-  console.log(body);
-  let jsonResponse = JSON.parse(body);
-  console.log(`I found ${jsonResponse.length} events:`);
-  jsonResponse.forEach((event) => {
-    console.log(`Venue: ${event.venue.name}`);
-    console.log(`Location: ${event.venue.city}, ${event.venue.country}`);
-    console.log(`Date: ${event.datetime}`);
-    console.log(`----------------------------------`);
+if (command === 'concert-this') {
+  let bandsInTownRequestUrl = `https://rest.bandsintown.com/artists/${params}/events?app_id=${keys.bandsInTown.appId}`;
+
+  request(bandsInTownRequestUrl, (error, response, body) => {
+    let jsonResponse = JSON.parse(body);
+
+    console.log(`I found ${jsonResponse.length} events:`);
+
+    jsonResponse.forEach((event) => {
+      console.log(`Venue: ${event.venue.name}`);
+      console.log(`Location: ${event.venue.city}, ${event.venue.country}`);
+      console.log(`Date: ${event.datetime}`);
+      console.log(`----------------------------------`);
+    });
   });
-});
+}
+
+if (command === 'spotify-this') {
+  const Spotify = require('node-spotify-api');
+
+  let spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
+
+  spotify.search({ type: 'track', query: params }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    data.tracks.items.forEach((trackInfo) => {
+      console.log("Artist:", trackInfo.artists.map(({ name }) => name).join(", "));
+      console.log("Name:", trackInfo.name);
+      console.log("Album:", trackInfo.album.name);
+      console.log("Preview URL:", trackInfo.preview_url);
+      console.log("--------------------------------");
+    });
+
+  });
+}
