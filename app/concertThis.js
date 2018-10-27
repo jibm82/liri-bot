7b844b64516499470211febfc822f5dc65d58ceb
Jorge Baez <jibm_82@yahoo.com.mx>
2018-10-27 17:06:34 -0500

    Handle empty param

--- app/concertThis.js
+++ app/concertThis.js
@@ -3,20 +3,26 @@ const moment = require("moment");
 const keys = require("../keys");
 
 function concertThis(searchParams, callback) {
+
   let bandsInTownRequestUrl = `https://rest.bandsintown.com/artists/${searchParams}/events?app_id=${keys.bandsInTown.appId}`;
   let concertThisResponse = "";
 
   request(bandsInTownRequestUrl, (error, response, body) => {
     let jsonResponse = JSON.parse(body);
 
-    concertThisResponse += `I found ${jsonResponse.length} events:\n\n`;
+    if (!jsonResponse.message) {
 
-    jsonResponse.forEach((event) => {
-      concertThisResponse += `Venue: ${event.venue.name}\n`;
-      concertThisResponse += `Location: ${event.venue.city}, ${event.venue.country}\n`;
-      concertThisResponse += `Date: ${moment(event.datetime).format("MM/DD/YYYY")}\n`;
-      concertThisResponse += "----------------------------------\n";
-    });
+      concertThisResponse += `I found ${jsonResponse.length} events:\n\n`;
+
+      jsonResponse.forEach((event) => {
+        concertThisResponse += `Venue: ${event.venue.name}\n`;
+        concertThisResponse += `Location: ${event.venue.city}, ${event.venue.country}\n`;
+        concertThisResponse += `Date: ${moment(event.datetime).format("MM/DD/YYYY")}\n`;
+        concertThisResponse += "----------------------------------\n";
+      });
+    } else {
+      concertThisResponse = jsonResponse.message;
+    }
 
     if (typeof callback === "function") {
       callback(concertThisResponse);
